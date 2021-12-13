ed2809db383f155686395bd1a130fcf095c8a58f
Augustin <apetre.pro@gmail.com>
2021-12-13 16:06:32 +0100

    temp

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hackaton12-21</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
+</head>
+
+<body>
+    <div class="container p-4">
+        <h1 class="text-center">BIENVENUE SUR</h1>
+        <div class="text-center">
+            <img class="p-4 img-fluid" src="assets/image/logo_chronomiam.png" alt="logo-chrono-miam">
+        </div>
+        <div class="text-center p-3">
+
+            <h3>Application de réservation de Micro-onde pour le centre de LA MANU du Havre.</h3>
+        </div>
+        <div class="text-center p-3">
+            <h3>
+                <p>Marre d'attendre pour rechauffer votre repas?</p>
+                <div class="text-center">
+                    <p>Avec CHRONO-MIAM, vous pourrez réserver le micro-onde pour manger avant les autres!</p>
+            </h3>
+        </div>
+    </div>
+<div class="text-center">
+    <form action="name.php">
+        <input class="text-white buttonNext justify-content-center" type="submit" value="Suivant">
+    </form>
+    </div>
+
+
+
+</body>
+
+</html>
